f939c278c7f2d570b3fd0189656a3138f8662a18
luisgalindosoto <73960357+luisgalindosoto@users.noreply.github.com>
2023-08-29 11:28:34 -0600

    otro comit

--- brc_tags/Form1.Designer.cs
+++ brc_tags/Form1.Designer.cs
@@ -1062,7 +1062,7 @@
             // btn_units
             // 
             this.btn_units.Image = global::brc_tags.Properties.Resources.UNITS64;
-            this.btn_units.Location = new System.Drawing.Point(535, 205);
+            this.btn_units.Location = new System.Drawing.Point(516, 280);
             this.btn_units.Name = "btn_units";
             this.btn_units.Size = new System.Drawing.Size(75, 74);
             this.btn_units.TabIndex = 68;
